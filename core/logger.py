1a6a23f50d47b232fec8e8dd32729d4455b64ac2
Andrew Click-Horn <andrewphorn@gmail.com>
2015-08-14 02:20:45 -0700

    preliminary logging restructure

--- core/logger.py
+++ core/logger.py
@@ -1,7 +1,21 @@
+﻿try:
+    from colorama import init, Fore, Back, Style
+    init()
+    colored = True
+except ImportError:
+    colored = False
+
+import datetime, time
 
 class LoggerClass(object):
-	def log(self,text):
-		print(text)
+    def __log(self,text):
+        ts = "[%s]" % datetime.date.today().strftime("%d-%m-%Y %H:%M:%S")
 
-	def error(self,text):
-		print('ERROR: %s' % text)
+    def log(self,text):
+        self.__log(text)
+
+    def error(self,text):
+        self.__log(text)
+
+    def debug(self,text):
+        self.__log(text)
